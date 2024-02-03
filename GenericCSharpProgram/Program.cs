623857b0f1a94e6d6d36bff99f275c8919fed9a5
DE-YU_H14 <delsin_yu@qq.com>
2024-02-04 04:33:21 +0800

    Format fix

--- GenericCSharpProgram/Program.cs
+++ GenericCSharpProgram/Program.cs
@@ -31,27 +31,27 @@ public partial class Program
 
     [Benchmark] public void SimpleType_TML() => SimpleType(ConstructTypeName_TML);
     [Benchmark] public void ModerateType_TML() => ModerateType(ConstructTypeName_TML);
-    [Benchmark] public void CrazyType_TML() => ModerateType(ConstructTypeName_TML);
+    [Benchmark] public void CrazyType_TML() => CrazyType(ConstructTypeName_TML);
 
     [Benchmark] public void SimpleType_JSDY() => SimpleType(ConstructTypeName_JSDY);
     [Benchmark] public void ModerateType_JSDY() => ModerateType(ConstructTypeName_JSDY);
-    [Benchmark] public void CrazyType_JSDY() => ModerateType(ConstructTypeName_JSDY);
+    [Benchmark] public void CrazyType_JSDY() => CrazyType(ConstructTypeName_JSDY);
 
     [Benchmark] public void SimpleType_JSDY_OPT() => SimpleType(ConstructTypeName_JSDY_OPT);
     [Benchmark] public void ModerateType_JSDY_OPT() => ModerateType(ConstructTypeName_JSDY_OPT);
-    [Benchmark] public void CrazyType_JSDY_OPT() => ModerateType(ConstructTypeName_JSDY_OPT);
+    [Benchmark] public void CrazyType_JSDY_OPT() => CrazyType(ConstructTypeName_JSDY_OPT);
 
     [Benchmark] public void SimpleType_JSDY_OPT2() => SimpleType(ConstructTypeName_JSDY_OPT2);
     [Benchmark] public void ModerateType_JSDY_OPT2() => ModerateType(ConstructTypeName_JSDY_OPT2);
-    [Benchmark] public void CrazyType_JSDY_OPT2() => ModerateType(ConstructTypeName_JSDY_OPT2);
+    [Benchmark] public void CrazyType_JSDY_OPT2() => CrazyType(ConstructTypeName_JSDY_OPT2);
 
     [Benchmark] public void SimpleType_JSDY_OPT3() => SimpleType(ConstructTypeName_JSDY_OPT3);
     [Benchmark] public void ModerateType_JSDY_OPT3() => ModerateType(ConstructTypeName_JSDY_OPT3);
-    [Benchmark] public void CrazyType_JSDY_OPT3() => ModerateType(ConstructTypeName_JSDY_OPT3);
+    [Benchmark] public void CrazyType_JSDY_OPT3() => CrazyType(ConstructTypeName_JSDY_OPT3);
 
     [Benchmark] public void SimpleType_ZERXZ() => SimpleType(ConstructTypeName_ZERXZ);
     [Benchmark] public void ModerateType_ZERXZ() => ModerateType(ConstructTypeName_ZERXZ);
-    [Benchmark] public void CrazyType_ZERXZ() => ModerateType(ConstructTypeName_ZERXZ);
+    [Benchmark] public void CrazyType_ZERXZ() => CrazyType(ConstructTypeName_ZERXZ);
 
     private static void SimpleType(Func<Type, string> call)
     {

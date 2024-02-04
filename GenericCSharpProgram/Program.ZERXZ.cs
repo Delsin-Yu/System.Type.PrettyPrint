fd9354ef8e75b5afc4703bfc55baaf30ff15a813
深渊の鸽子 <ghzero97@gmail.com>
2024-02-04 03:43:58 +0100

    Add Benchmark.Net example workflow and optimize ZERXZ program

--- GenericCSharpProgram/Program.ZERXZ.cs
+++ GenericCSharpProgram/Program.ZERXZ.cs
@@ -8,7 +8,9 @@ public partial class Program
     [Benchmark] public void SimpleType_ZERXZ() => SimpleType(ConstructTypeName_ZERXZ);
     [Benchmark] public void ModerateType_ZERXZ() => ModerateType(ConstructTypeName_ZERXZ);
     [Benchmark] public void CrazyType_ZERXZ() => CrazyType(ConstructTypeName_ZERXZ);
-
+    [Benchmark] public void SimpleType_ZERXZ_OPT() => SimpleType(ConstructTypeName_ZERXZ_OPT);
+    [Benchmark] public void ModerateType_ZERXZ_OPT() => ModerateType(ConstructTypeName_ZERXZ_OPT);
+    [Benchmark] public void CrazyType_ZERXZ_OPT() => CrazyType(ConstructTypeName_ZERXZ_OPT);
 
     public static string ConstructTypeName_ZERXZ(Type type)
     {
